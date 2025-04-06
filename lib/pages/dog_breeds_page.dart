e1c860cb39474f6a2e1f82f0ef0ee9c495bf8da1
androbene <androbene@ua.fm>
2025-04-06 19:21:27 +0300

    ADD breeds provider logic

--- lib/pages/dog_breeds_page.dart
+++ lib/pages/dog_breeds_page.dart
@@ -1,99 +1,69 @@
-import 'dart:convert';
+
+import 'package:dogapp/pages/dog_breeds_provider.dart';
 import 'package:dogapp/res/app_res.dart';
 import 'package:dogapp/widgets/preloader_subtitle.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
-class DogBreedsPage extends StatefulWidget {
+class DogBreedsPage extends StatelessWidget {
   const DogBreedsPage({super.key});
 
   @override
-  State<DogBreedsPage> createState() => _DogBreedsPageState();
-}
+  Widget build(BuildContext context) {
 
-class _DogBreedsPageState extends State<DogBreedsPage> {
-  List<String> breeds = [];
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => DogBreedsProvider()..fetchBreeds(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              const SizedBox(width: 44),
+              const Expanded(
+                child: Hero(
+                  tag: 'hero1',
+                  child: PreloaderSubtitle(),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: AppColors.mainBg,
+        ),
+        backgroundColor: AppColors.mainBg,
+        body: Consumer<DogBreedsProvider>(
+            builder: (cont, dbp, p2) => dbp.breeds.isEmpty
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    itemCount: dbp.breeds.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        alignment: Alignment.centerLeft,
+                        height: 48,
+                        margin: const EdgeInsets.only(bottom: 12),
+                        decoration: AppDeco.mainDeco,
+                        child: Text(
+                          _capitalizeFirstLetter(dbp.breeds[index]),
+                          style: AppTypo.body1,
+                        ),
+                      );
+                    },
+                  )),
+      ),
+    );
+  }
 
   String _capitalizeFirstLetter(String text) {
     // todo extract
     if (text.isEmpty) return text;
     return text[0].toUpperCase() + text.substring(1);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchBreeds();
-  }
-
-  Future<void> fetchBreeds() async {
-    try {
-      final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/list/all'));
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final Map<String, dynamic> breedMap = data['message'];
-
-        setState(() {
-          breeds = breedMap.keys.toList();
-        });
-      } else {
-        setState(() {
-          breeds = ['Server error'];
-        });
-      }
-    } catch (e) {
-      setState(() {
-        breeds = ['No internet'];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            const SizedBox(width: 44),
-            const Expanded(
-              child: Hero(
-                tag: 'hero1',
-                child: PreloaderSubtitle(),
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.check_circle_outline,
-                color: Colors.white,
-                size: 40,
-              ),
-            ),
-          ],
-        ),
-        backgroundColor: AppColors.mainBg,
-      ),
-      backgroundColor: AppColors.mainBg,
-      body: breeds.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              itemCount: breeds.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  alignment: Alignment.centerLeft,
-                  height: 48,
-                  margin: const EdgeInsets.only(bottom: 12),
-                  decoration: AppDeco.mainDeco,
-                  child: Text(
-                    _capitalizeFirstLetter(breeds[index]),
-                    style: AppTypo.body1,
-                  ),
-                );
-              },
-            ),
-    );
   }
 }
---
+++ lib/pages/dog_breeds_provider.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class DogBreedsProvider extends ChangeNotifier {
+  List<String> breeds = [];
+
+  Future<void> fetchBreeds() async {
+    try {
+      await Future.delayed(Duration(seconds: 1));
+      final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/list/all'));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final Map<String, dynamic> breedMap = data['message'];
+
+        breeds = breedMap.keys.toList();
+        notifyListeners();
+      } else {
+        breeds = ['Server error'];
+        notifyListeners();
+      }
+    } catch (e) {
+      breeds = ['No internet'];
+      notifyListeners();
+    }
+  }
+}
